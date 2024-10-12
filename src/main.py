0eed5229547fa2cf57e119b36f46de9ea8e20eed
Locko2901 <simon.lockowandt@gmx.de>
2024-10-12 16:50:17 +0200

    removed debug code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 from gui.gui import YouTubeDownloaderGUI
 
 if __name__ == '__main__':
-    try:
-        app = YouTubeDownloaderGUI()
-        app.run()
-    except Exception as e:
-        print(e)
-        input("Press Enter to exit...")
+    app = YouTubeDownloaderGUI()
+    app.run()
+    
